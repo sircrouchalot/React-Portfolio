39b3a69c06b383e827cc356e175d73d34737706c
Ashley Chambers <acrouch06@gmail.com>
2020-09-10 11:19:17 -0500

    added Home Route path

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
 import About from "./pages/About";
 import Portfolio from "./pages/Portfolio";
 import Contact from "./pages/Contact";
@@ -16,7 +17,8 @@ function App() {
         <NavBar />
 
         <Switch>
-                <Route exact path={["/", "/about"]} component={About} />
+                <Route exact path={["", "/"]} component={Home} />
+                <Route exact path="/about" component={About} />
                 <Route exact path="/portfolio" component={Portfolio} />
                 <Route exact path="/contact" component={Contact} />
                 
